ba4498ddf536fce4aebe05b8c91f19c7fc48c031
Chanakya-TS <66557279+Chanakya-TS@users.noreply.github.com>
2022-03-28 12:45:42 +0530

    Added AI Bot

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 let WIDTH = 400, HEIGHT = 400;
 
 let cnv;
+let scorecnv;
 
-let board = [];
+let board = [], boardC = [];
 let winHorz = [], winVert = [];
 let winAsc = [], winDsc = [];
+let rowVals = [];
 let BWIDTH = 7, BHEIGHT = 6;
 let CSIZE_H = HEIGHT / BHEIGHT, CSIZE_W = WIDTH / BWIDTH;
 let COINRAD = 40;
@@ -20,24 +22,36 @@ let BLUE = [33, 161, 255];
 let redTurn = true;
 let gameOver = false;
 
+let useAI = true;
+let mxDepth = 9;
+
+let cache = {};
+
 function centerCanvas(){
   cnv.position((windowWidth - WIDTH) / 2, (windowHeight - HEIGHT) / 2);
 }
 
 function resetSketch(){
   board = [];
+  boardC = [];
   winHorz = [];
   winVert = [];
   winAsc = [];
   winDsc = [];
+  rowVals = [];
+  for(let i=0; i<BWIDTH; i++){
+    rowVals.push("NA");
+  }
   for(let i=0; i<BHEIGHT; i++){
     board[i] = [];
+    boardC[i] = [];
     winHorz[i] = [];
     winVert[i] = [];
     winAsc[i] = [];
     winDsc[i] = [];
     for(let j=0; j<BWIDTH; j++){
       board[i][j] = '';
+      boardC[i][j] = '';
       winHorz[i][j] = 0;
       winVert[i][j] = 0;
       winAsc[i][j] = 0;
@@ -61,7 +75,7 @@ function setup() {
   restart.style('text-align', 'center');
   restart.style('padding-top', '10px');
   restart.style('font-family', 'Arial, Helvetica, sans-serif')
-  cnv = createCanvas(WIDTH, HEIGHT);
+  cnv = createCanvas(WIDTH, HEIGHT+100);
   centerCanvas();
   resetSketch();
 }
@@ -72,10 +86,15 @@ function windowResized(){
 
 let prevHoverCol = -1;
 
-function getLatestActive(col){
+function getLatestActive(col, useCopy = false){
   let result = [-1, -1];
   for(let i=BHEIGHT-1; i>=0; i--){
-    if(board[i][col] != 'r' && board[i][col] != 'y'){
+    if(!useCopy && board[i][col] != 'r' && board[i][col] != 'y'){
+      result[0] = i;
+      result[1] = col;
+      break;
+    }
+    if(useCopy && boardC[i][col] != 'r' && boardC[i][col] != 'y'){
       result[0] = i;
       result[1] = col;
       break;
@@ -85,6 +104,7 @@ function getLatestActive(col){
 }
 
 function draw() {
+  background(255, 255, 255);
   fill(BLUE);
   strokeWeight(0);
   rect(0, 0, WIDTH, HEIGHT, 20);
@@ -173,11 +193,20 @@ function draw() {
       }
     }
   }
+  for(let i=0; i<BWIDTH; i++){
+    textSize(20);
+    fill(0, 0, 0);
+    text(rowVals[i], i * CSIZE_W + CSIZE_W * 0.5 , HEIGHT + 25);
+    textAlign('center');
+  }
 }
 
-function isAvail(col){
+function isAvail(col, useCopy = false){
   for(let row=BHEIGHT-1; row >= 0; row--){
-    if(board[row][col] != 'r' && board[row][col] != 'y'){
+    if(!useCopy && board[row][col] != 'r' && board[row][col] != 'y'){
+      return true;
+    }
+    if(useCopy && boardC[row][col] != 'r' && boardC[row][col] != 'y'){
       return true;
     }
   }
@@ -193,17 +222,20 @@ function placeCoin(col, coin){
   }
 }
 
-function checkWin(coin){
+function checkWin(coin, draw = true, useCopy = false){
   // Horizontal
   for(let i=0; i<BHEIGHT; i++){
     for(let j=0; j<BWIDTH-3; j++){
       let win = true;
       for(let k=1; k<4; k++){
-        win &= board[i][j+k] == board[i][j+k-1] && board[i][j] == coin;
+        if(!useCopy)
+          win &= board[i][j+k] == board[i][j+k-1] && board[i][j] == coin;
+        else 
+          win &= boardC[i][j+k] == boardC[i][j+k-1] && boardC[i][j] == coin;
       }
       if(win){
-        print("HORIZONTAL WIN");
-        winHorz[i][j] = 1;
+        if(draw)
+          winHorz[i][j] = 1;
         return true;
       }
     }
@@ -213,11 +245,14 @@ function checkWin(coin){
     for(let j=0; j<BWIDTH; j++){
       let win = true;
       for(let k=1; k<4; k++){
-        win &= board[i+k][j] == board[i+k-1][j] && board[i][j] == coin;
+        if(!useCopy)
+          win &= board[i+k][j] == board[i+k-1][j] && board[i][j] == coin;
+        else 
+          win &= boardC[i+k][j] == boardC[i+k-1][j] && boardC[i][j] == coin;
       }
       if(win){
-        print("VERTICAL WIN");
-        winVert[i][j] = 1;
+        if(draw)
+          winVert[i][j] = 1;
         return true;
       }
     }
@@ -227,11 +262,14 @@ function checkWin(coin){
     for(let j=0; j<BWIDTH-3; j++){
       let win = true;
       for(let k=1; k<4; k++){
-        win &= board[i+k][j+k] == board[i+k-1][j+k-1] && board[i][j] == coin;
+        if(!useCopy)
+          win &= board[i+k][j+k] == board[i+k-1][j+k-1] && board[i][j] == coin;
+        else
+          win &= boardC[i+k][j+k] == boardC[i+k-1][j+k-1] && boardC[i][j] == coin;
       }
       if(win){
-        print("DSSC WIN");
-        winDsc[i][j] = 1;
+        if(draw)
+          winDsc[i][j] = 1;
         return true;
       }
     }
@@ -241,25 +279,135 @@ function checkWin(coin){
     for(let j=0; j<BWIDTH-3; j++){
       let win = true;
       for(let k=1; k<4; k++){
-        win &= board[i-k][j+k] == board[i-k+1][j+k-1] && board[i][j] == coin;
+        if(!useCopy)
+          win &= board[i-k][j+k] == board[i-k+1][j+k-1] && board[i][j] == coin;
+        else
+          win &= boardC[i-k][j+k] == boardC[i-k+1][j+k-1] && boardC[i][j] == coin;
       }
       if(win){
-        print("ASC WIN");
-        winAsc[i][j] = 1;
-        print(winHorz[i][j]);
+        if(draw)
+          winAsc[i][j] = 1;
         return true;
       }
     }
   }
+  return false;
+}
+
+function chooseBest(player, depth, alpha, beta){
+    if([boardC, player] in cache){
+      return cache[[boardC, player]];
+    }
+    if(checkWin('y', false, true)){
+      return 100 - depth;
+    } else if(checkWin('r', false, true)){
+      return -100 - depth;
+    }
+    let moves = [];
+    for(let i=0; i<BWIDTH; i++){
+      if(isAvail(i, true)){
+        moves.push(i);
+      }
+    }
+    if(moves.length == 0) return 0;
+    if(depth >= mxDepth) return 0;
+    // Maximising
+    let best;
+    if(player){
+      best = -1e9;
+      for(let i=0; i<moves.length; i++){
+        let row = getLatestActive(moves[i], true)[0];
+        boardC[row][moves[i]] = 'y';
+        best = max(best, chooseBest(0, depth+1, alpha, beta));
+        boardC[row][moves[i]] = '';
+        alpha = max(alpha, best);
+        if(alpha >= beta){
+          break;
+        }
+      }
+    } else {
+      best =  1e9;
+      for(let i=0; i<moves.length; i++){
+        let row = getLatestActive(moves[i], true)[0];
+        boardC[row][moves[i]] = 'r';
+        best = min(best, chooseBest(1, depth+1, alpha, beta));
+        boardC[row][moves[i]] = '';
+        beta = min(beta, best);
+        if(alpha >= beta){
+          break;
+        }
+      }
+    }
+    cache[[boardC,player]] = best;
+    return best;
+}
+
+function minMax(){
+    let moves = [];
+    for(let i=0; i<BWIDTH; i++){
+      if(isAvail(i, true)){
+        moves.push(i);
+      }
+    }
+    for(let i=0; i<BWIDTH; i++){
+      rowVals[i] = 'NA';
+    }
+    let bestMoves = [], bestVal = -1e9;
+    for(let i=0; i<moves.length; i++){
+      let row = getLatestActive(moves[i], true)[0];
+      boardC[row][moves[i]] = 'y';
+      let cur = chooseBest(0, 0, -1e9, 1e9);
+      rowVals[moves[i]] = cur;
+      boardC[row][moves[i]] = '';
+      if(bestVal == cur){
+        bestMoves.push(moves[i]);
+      } else if(bestVal < cur){
+        bestVal = cur;
+        bestMoves = [];
+        bestMoves.push(moves[i]);
+      }
+    }
+    return random(bestMoves);
+}
+
+function aiMove(){
+  if(!useAI){
+    let moves = [];
+    for(let i=0; i<BWIDTH; i++){
+      if(isAvail(i)){
+        moves.push(i);
+      }
+    }
+    return random(moves);
+  } else {
+    for(let i=0; i<BHEIGHT; i++){
+      for(let j=0; j<BWIDTH; j++){
+        if(board[i][j] == 'r') boardC[i][j] = 'r';
+        else if(board[i][j] == 'y') boardC[i][j] = 'y';
+      }
+    }
+    return minMax();
+  }
 }
 
 function mouseClicked(){
-  if(!gameOver){
+  if(!gameOver && mouseX >= 0 && mouseX <= WIDTH){
     let col = int(mouseX / (WIDTH / BWIDTH));
     let coin = 'r';
-    if(!redTurn) coin = 'y';
     if(isAvail(col)){
       placeCoin(col, coin);
+      if(checkWin('r')){
+        gameOver = true;
+      }
+      else if(checkWin('y')){
+        gameOver = true;
+      }
+      redTurn ^= 1;
+    }
+    
+    if(!redTurn && !gameOver){
+      let result = aiMove();
+      placeCoin(result, 'y');
       if(checkWin('r')){
         gameOver = true;
       }
